9f0317e2087d6a684a401f1c00f594f473493602
Gerald Nunn <gnunn1@users.noreply.github.com>
2017-08-31 19:33:59 -0400

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 
 <!--<![endif]-->
   <head>
-    <title>Basic - PatternFly Test</title>
+    <title>Basic - PatternFly/title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="../dist/img/favicon.ico">
     <!-- iPad retina icon -->
